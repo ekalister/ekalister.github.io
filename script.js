0b0e3bf278002cfa91da4012326dd9b8b6673dde
unknown <kalister.edi99@gmail.com>
2021-11-07 21:49:24 +0100

    EmailJS

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ $(document).ready(function () {
     $("header").toggleClass("toggle");
   });
 });
+
+function send() {
+  emailjs
+    .send("service_pl67x3e", "template_2eycbz8", {
+      to_name: "Edi",
+      from_name: document.querySelector("#from_name").value,
+      message: document.querySelector("#message").value,
+      email_to: "ekalister@student.unipu.hr",
+    })
+    .then(
+      (response) => {
+        console.log("succes", response.status);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+}
+
+document.querySelector(".send").addEventListener("click", send);
